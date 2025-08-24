069284e2b1cdfca5bdae11bd091dd1ba9bc08b0d
nyassyal1 <58556182+nyassyal@users.noreply.github.com>
2025-08-24 16:38:23 -0400

    Add Playwright test ID demo and update config
    Added myfirstapp.html with a button using 'pw-test-id' attribute for test targeting. Updated Playwright config to use 'pw-test-id' as the testIdAttribute and enabled screenshots. Modified example.spec.ts to include comments and adjust timeouts for better test reliability.

--- playwright-test-demo/tests/example.spec.ts
+++ playwright-test-demo/tests/example.spec.ts
@@ -1,14 +1,29 @@
 import { test, expect } from '@playwright/test';
+import path from 'path';
 
+// test.skip('has title', async ({ page }) => { // skips this test case
+// test.only('has title', async ({ page }) => { // only run this test case
+// test.fail('has title', async ({ page }) => { // mark this test as failed
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/',{timeout:60000});
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle(/Playwright/),{timeout:10000};
+  // const timestamp = Date.now();
+  // await page.screenshot({ path: `test-results/screenshots/screenshot_${timestamp}.png`, fullPage: true });
+  // await page.video().saveAs(`test-results/videos/video_${timestamp}.webm`);
+
+  
 });
 
+// test(`should click login button`, async({page})=>{
+//   await page.goto('http://127.0.0.1:5500/tests/myfirstapp.html');
+//   await page.getByTestId(`login-button`).click(); // This will fail - when the button is not found; now it will not because the correct test id is used(data-testid and not pw-test-id)
+
+// })
+
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto('https://playwright.dev/');  
 
   // Click the get started link.
   await page.getByRole('link', { name: 'Get started' }).click();
